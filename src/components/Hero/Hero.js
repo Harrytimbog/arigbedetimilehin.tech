d1e38a276dd8cbfe76b9cda87d83914a2258ccc9
Timilehin Arigbede <Harriemannie@gmail.com>
2021-12-06 14:23:52 +0100

    Create Hero session

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        I Am A <br />
+        Fullstack Developer
+      </SectionTitle>
+      <SectionText>
+        I can help you build web applications and also use my development skills to take your ideas to the next level by building awesome apps.
+      </SectionText>
+      <Button onClick={props.handleClick}>Learn More.</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
