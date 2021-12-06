92007f1c28c75994ec0f5563d1518ac4c6582966
Timilehin Arigbede <Harriemannie@gmail.com>
2021-12-06 16:15:27 +0100

    Code Project session

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = (props) => (
         Fullstack Developer
       </SectionTitle>
       <SectionText>
-        I can help you build web applications and also use my development skills to take your ideas to the next level by building awesome apps.
+        I can help you build web applications and also use my web development skills to take your ideas to the next level by building awesome apps.
       </SectionText>
       <Button onClick={props.handleClick}>Learn More.</Button>
     </LeftSection>
